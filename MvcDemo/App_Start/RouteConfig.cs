83310e80bef64b859397b01dafc5d8876448df96
Mert Çakar <mertcakar.es@gmail.com>
2018-12-12 21:49:04 +0300

    The error is fixed for student/detail without id

--- MvcDemo/App_Start/RouteConfig.cs
+++ MvcDemo/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MvcDemo
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "StudentDetailsRoute",
+                url: "Student/Details",
+                defaults: new { controller = "Student", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
