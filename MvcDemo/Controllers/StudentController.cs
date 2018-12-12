d8e13ba1ca54dc786cabcdf71d8415d49dae3504
Mert Çakar <mertcakar.es@gmail.com>
2018-12-12 12:53:04 +0300

    Change is made on Student model

--- MvcDemo/Controllers/StudentController.cs
+++ MvcDemo/Controllers/StudentController.cs
@@ -16,13 +16,13 @@ namespace MvcDemo.Controllers
         }
 
         List<Student> studentList = new List<Student>{
-                            new Student() { StudentId = 1, Name = "John", Age = 18 } ,
-                            new Student() { StudentId = 2, Name = "Steve",  Age = 21 } ,
-                            new Student() { StudentId = 3, Name = "Bill",  Age = 25 } ,
-                            new Student() { StudentId = 4, Name = "Ram" , Age = 20 } ,
-                            new Student() { StudentId = 5, Name = "Ron" , Age = 31 } ,
-                            new Student() { StudentId = 4, Name = "Chris" , Age = 17 } ,
-                            new Student() { StudentId = 4, Name = "Rob" , Age = 19 }
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
                         };
     }
 }
--- MvcDemo/Models/Student.cs
+++ MvcDemo/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace MvcDemo.Models
     public class Student
     {
         public int StudentId { get; set; }
-        public string Name { get; set; }
+
+        [Display(Name="Name")]
+        public string StudentName { get; set; }
+
         public int Age { get; set; }
     }
 }
