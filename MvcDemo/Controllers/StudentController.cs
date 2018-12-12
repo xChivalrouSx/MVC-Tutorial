15b691666d5d3c00f495960fe8642c4d8bf34b4a
Mert Çakar <mertcakar.es@gmail.com>
2018-12-12 12:27:13 +0300

    Student model is created

---
+++ MvcDemo/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using MvcDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+        List<Student> studentList = new List<Student>{
+                            new Student() { StudentId = 1, Name = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, Name = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, Name = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, Name = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, Name = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, Name = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, Name = "Rob" , Age = 19 }
+                        };
+    }
+}
---
+++ MvcDemo/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
